3213b2ce8ad4df96c11a9fd7bc68542468f4c209
Romaan Naeem <romaan.naeem@outlook.com>
2018-08-21 12:35:38 -0400

    init commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/test', (req, res) => {
+  res.send({ test: 'route' });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log('Webhook server is on'));
